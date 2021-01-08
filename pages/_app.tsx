99dc0adf23e7b4434886c05d2f12eaa3a1200b5e
Hanzhi Yin <yinhanzhi@gmail.com>
2021-01-07 21:44:42 -0600

    project init

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import type { AppProps } from 'next/app';
+import '../styles/globals.scss';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
