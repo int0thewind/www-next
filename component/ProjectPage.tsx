a0b1e5e7725104c63ea1ae744337acb66cd27af8
Hanzhi Yin <yinhanzhi@gmail.com>
2021-01-08 19:02:49 -0600

    project info works. Need to tweek UI

--- component/ProjectPage.tsx
+++ component/ProjectPage.tsx
@@ -1,11 +1,46 @@
+import { useState } from 'react';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
 import { ProjectInfo } from '../lib';
 
 type ProjectCardProps = {
   data: ProjectInfo;
 };
 
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 function ProjectCard({ data }: ProjectCardProps) {
-  return <p>Project Card Works!</p>;
+  const {
+    title, isMain, year, month, tags, desc, shortDesc, colab, links,
+  } = data;
+  const dateString = `${months[month]} ${year}`;
+  const [displayDesc, setDisplayDesc] = useState(false);
+  const toggleDisplayDesc = () => setDisplayDesc(!displayDesc);
+  return (
+    <div className="project-card">
+      <h2>{title}</h2>
+      <div className="project-card-tags">
+        {!isMain || <Badge pill variant="danger">Major Work</Badge>}
+        <Badge pill variant="warning">{dateString}</Badge>
+        {tags.map((tag) => <Badge key={tag} pill variant="info">{tag}</Badge>)}
+      </div>
+      <p>
+        {displayDesc ? desc : shortDesc}
+      </p>
+      <Button onClick={toggleDisplayDesc} variant="link" size="sm">
+        {displayDesc ? 'Show Less' : 'Show More'}
+      </Button>
+      {Object.keys(links).map((key) => <Button key={key} href={links[key]}>{key}</Button>)}
+      {!colab.length || (
+      <>
+        <h5>Collaborators</h5>
+        <ul>
+          {colab.map(({ name, contact }) => <li key={name}>{`${name}: ${contact}`}</li>)}
+        </ul>
+      </>
+      )}
+    </div>
+  );
 }
 
 type ProjectPageProps = {
@@ -13,5 +48,9 @@ type ProjectPageProps = {
 };
 
 export default function ProjectPage({ data }: ProjectPageProps) {
-  return <p>Project Page Works!</p>;
+  return (
+    <div className="project-page">
+      {data.map((d) => <ProjectCard data={d} />)}
+    </div>
+  );
 }
