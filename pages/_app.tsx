59010024f61ab1efdfa654cad88082e5c91d7850
Hanzhi Yin <yinhanzhi@gmail.com>
2021-01-10 11:12:48 -0600

    add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Footer, Header } from '../component';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Hanzhi Yin</title>
+      </Head>
       <Header />
       <main>
         <Component {...pageProps} />
